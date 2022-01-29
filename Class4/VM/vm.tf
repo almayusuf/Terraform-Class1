7879ee37a307ffff234cf018c2a6adb7a34e3851
almayusuf <ysfalma@gmail.com>
2022-01-29 21:09:36 +0000

    bucket

--- Class4/VM/vm.tf
+++ Class4/VM/vm.tf
@@ -13,6 +13,10 @@ resource "google_compute_instance" "vm_instance" {
 	}
 	labels = var.labels
 	tags = [var.vm_config["network_tags"]]
+    metadata_startup_script = file("startup.sh")
+	metadata = {
+		ssh-keys = "debian:${file("~/.ssh/id_rsa.pub")}bob:${file("~/.ssh/id_rsa.pub")}"
+    }
 }
 
 resource "google_compute_firewall" "allow-http" {
